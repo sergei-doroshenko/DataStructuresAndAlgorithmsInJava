ca108e8d89c37023a59404d5999d8fcf4e32af95
sergei-doroshenko <6823298@gmail.com>
2015-01-27 19:15:49 +0200

    Task of team composition solved

--- src/main/java/chap06/ProgrammProjects/TeamMaker.java
+++ src/main/java/chap06/ProgrammProjects/TeamMaker.java
@@ -1,46 +1,53 @@
 package chap06.ProgrammProjects;
 
-/**
- * Created by user on 27.01.2015.
+/** Creates teams of k from n-members
+ * Created by Sergei Doroshenko on 27.01.2015.
+ * According to theorem (n, k) = (n – 1, k – 1) + (n – 1, k)
+ * so we creates two branches:
+ * 1st - (n – 1, k – 1)
+ * 2nd - (n – 1, k)
+ *               (5,3)
+ *                 |
+ *        |                  |
+ *      (4,2)              (4,3)
+ *        |                  |
+ *    |       |        |          |
+ *  (3,1)   (3,2)    (3,2)      (3,3)
+ *  ... and so on ...
  */
 public class TeamMaker {
-    char[] range = new char[] {'A', 'B', 'C', 'D', 'E'};
+    public static int COUNTER;
+    public static int TEAMS;
 
-    /*public static void showTeams(int n, int k, String team, char next) {
-        if(k == 0) {
-            System.out.println("team = " + team);
+    public static void showTeams(int n, int k, String team, char next) {
+        COUNTER++;
+//        System.out.printf("(%d, %d) %3s '%s' RC: %2d\n", n, k, team, next, COUNTER);
+        /*  Base conditions  */
+        if(k == 1 && n == 1) {
+            TEAMS++;
+            System.out.println("team = " + team + next); // return team string with next char
             return;
 
+        } else if (k == 0) {
+            TEAMS++;
+            System.out.println("team = " + team);       // return team string without next char
+            return;                                     // that's mean all three chars appended to
+                                                        // team string (k = 3 -> 2 -> 1 -> 0)
         } else if (n < k) {
             return;
 
         } else {
 
-            showTeams(n - 1,k - 1,team + next, next++ );
+            showTeams(n - 1,k - 1,team + next, ++next ); // recursively calls 1st tree branch
 
-            showTeams(n - 1, k, team, next++);
-        }
-    }*/
-
-    public static void showTeams(int n, int k, String team, char next) {
-        System.out.printf("n: %2d k: %2d %3s %s\n", n, k, team, next);
-
-        if(k == 1 && n == 1) {
-            //System.out.println("team = " + team + next);
-            return;
-
-        } else if (k == 0 || n < k) {
-            return;
-
-        } else {
-
-            showTeams(n - 1,k - 1,team + next, next++ );
-
-            showTeams(n - 1, k, team, next++);
+            showTeams(n - 1, k, team, next);             // recursively calls 2nd tree branch
         }
     }
 
     public static void main (String[] args) {
         showTeams(5, 3, "", 'A');
+        System.out.println("===============================================");
+        System.out.println("Teams: " + TEAMS);
+        System.out.println("Recursive calls: " + COUNTER);
     }
 }
